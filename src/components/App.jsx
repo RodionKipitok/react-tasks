91a0ea00f5c64e3ee209c870cffd25906db2b3cb
admin <admin@admins-MacBook-Air.local>
2024-04-21 22:55:15 -0700

    added new task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
+
+// №2
+// Даны два инпута, кнопка и абзац. Пусть в инпуты вводятся даты в формате 2025-12-31. По нажатию на кнопку найдите разницу между датами в днях и результат выведите в абзац.
+
 export const App = () => {
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [result, setResult] = useState('');
+  console.log('i1 :', input1);
+  console.log('i2 :', input2);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <input
+        type="date"
+        onChange={event => {
+          setInput1(event.target.value);
+        }}
+      />
+      <input
+        type="date"
+        onChange={event => {
+          setInput2(event.target.value);
+        }}
+      />
+      <button>click me</button>
+      <p>{result}</p>
     </div>
   );
 };
