c27f3551a9a7eb14b0f08fbf6e390c6ff07a17fb
admin <admin@admins-MacBook-Air.local>
2024-04-23 23:13:08 -0700

    added new task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,29 +3,57 @@ import { useState } from 'react';
 // №2
 // Даны два инпута, кнопка и абзац. Пусть в инпуты вводятся даты в формате 2025-12-31. По нажатию на кнопку найдите разницу между датами в днях и результат выведите в абзац.
 
+// export const App = () => {
+//   const [input1, setInput1] = useState('');
+//   const [input2, setInput2] = useState('');
+//   const [result, setResult] = useState('');
+//   console.log('i1 :', input1);
+//   console.log('i2 :', input2);
+
+//   return (
+//     <div>
+//       <input
+//         type="date"
+//         onChange={event => {
+//           setInput1(event.target.value);
+//         }}
+//       />
+//       <input
+//         type="date"
+//         onChange={event => {
+//           setInput2(event.target.value);
+//         }}
+//       />
+//       <button>click me</button>
+//       <p>{result}</p>
+//     </div>
+//   );
+// };
+
 export const App = () => {
-  const [input1, setInput1] = useState('');
-  const [input2, setInput2] = useState('');
-  const [result, setResult] = useState('');
-  console.log('i1 :', input1);
-  console.log('i2 :', input2);
+  const [inputValue, setInputValue] = useState('');
+  const [sumNum, setSumNum] = useState('');
+
+  // function calculatesTheSumOfTheNumbers() {
+  //   const parseStrInNum = parseInt(inputValue);
+  //   const arrNum = parseStrInNum.split('');
+  //   const sumOfNumbers = arrNum.reduce((acc, number) => acc + number, 0);
+
+  //   return sumOfNumbers;
+  // }
+
+  // console.log(calculatesTheSumOfTheNumbers());
 
   return (
     <div>
       <input
-        type="date"
-        onChange={event => {
-          setInput1(event.target.value);
+        type="number"
+        onChange={e => {
+          setInputValue(e.target.value);
         }}
+        value={inputValue}
       />
-      <input
-        type="date"
-        onChange={event => {
-          setInput2(event.target.value);
-        }}
-      />
-      <button>click me</button>
-      <p>{result}</p>
+      <p>{sumNum}</p>
     </div>
   );
 };
